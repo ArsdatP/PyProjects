371d8e7a71dc6a519d8edaea939d3ade2e8656ea
RaphaelT <zerkausarsdat@gmail.com>
2023-11-06 13:47:42 -0500

    part 2

--- project1.py
+++ project1.py
@@ -1,10 +1,66 @@
 import random
 
+SCORE = 0
+
+print("This is a game of Pig! In order to win your die must roll a 1. ")
+print("if you wish to keep playing and risk all your balance, do it at your own accord.")
+
 def roll():
     die = random.randrange(1,6)
-
     return die
 
-test = roll()
 
-print(test)
+def play():
+    global SCORE
+
+    while True:
+        die = roll()
+        if die == 1:
+            print(f"You rolled a {die}. You lost all your points")
+            break
+        else:
+            SCORE += die
+
+            print(f"You rolled a {die}.")
+            print(f"Your current score: {SCORE}")
+
+            reply = input("Do you want to roll again (Y/N)? ").lower()
+            if reply != "y":
+                break
+
+
+
+
+def menu():
+    global SCORE
+
+    while True:
+        print("Welcome to my game of Pig!")
+        print("If you would like to play, press `Y` or `N` to quit ")
+        reply = input("> ")
+        reply = reply.lower()
+
+ 
+        if reply == "y":
+            score = 0
+            play()
+            print(f"Your total score: {score}")
+        elif reply == "n":
+            print("Bye Bye")
+            break
+        else:
+            print("Invalid Entry, try again.\n")
+
+
+
+
+
+menu()
+
+
+
+
+
+
+#test = roll()
+#print(test)
